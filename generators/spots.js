2a3f5af111b81a2ed58a52d49f1539bccab8c117
Paganini-Thurler <127864316+Paganini-Thurler@users.noreply.github.com>
2026-03-13 02:42:31 -0300

    Create spots.js

---
+++ generators/spots.js
@@ -0,0 +1,51 @@
+/* 
+Each of the arrays contain elements that can generate a random skate spot name by combining
+a random element from each.
+*/
+
+// Adjectives
+module.exports.descriptors = [
+    'Gnarly',
+    'Steez',
+    'Gritty',
+    'Sketchy',
+    'Buttery',
+    'Rad',
+    'Crusty',
+    'Technical',
+    'Heavy',
+    'Classic',
+    'Underground',
+    'Stoked',
+    'Switch',
+    'Fakie',
+    'Frontside',
+    'Backside',
+    'Smooth',
+    'Raw'
+];
+
+// Places
+module.exports.places = [
+    'Plaza',
+    'Bowl',
+    'Halfpipe',
+    'Ledge',
+    'Stair-set',
+    'Rail',
+    'Bank',
+    'Quarterpipe',
+    'Skatepark',
+    'Ditch',
+    'Parking Lot',
+    'Empty Pool',
+    'Loading Dock',
+    'Gap',
+    'Hubba',
+    'Curb',
+    'DIY',
+    'Warehouse',
+    'Street Spot',
+    'Court',
+    'Alley'
+];
