68664c827a084445d034b261687fd475aa766027
Paganini-Thurler <127864316+Paganini-Thurler@users.noreply.github.com>
2026-03-21 03:23:47 -0300

    Middleware update

--- index.js
+++ index.js
@@ -22,11 +22,12 @@ app.use(methodOverride("_method"));
 const mongoose = require("mongoose");
 // Schemas
 const Spot = require("./models/spot");
+const Review = require("./models/review");
 const { request } = require("http");
 
 // Joi Js Validators
 const Joi = require("joi");
-
+const {spotSchema, reviewSchema} = require("./validators/joiSchemas")
 // Endpoint error class
 const EndpointError = require("./errors/EndpointError");
 
@@ -40,11 +41,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/sk8tt')
 
 // Endpoints
 
-// Home 
+// Home (GET) 
 app.get("/", (request, response) =>{
     response.render("sk8tt.ejs");
 });
-// Get all Spots 
+// (GET) all Spots endpoint
 app.get("/spots", async (request, response, next) =>{
     //Async functions require next() in order to work properly 
     try{
@@ -55,30 +56,28 @@ app.get("/spots", async (request, response, next) =>{
     }
   
 });
-// Create a spot form
+// (GET) Create a spot form endpoint
 app.get("/spots/new", (request, response) =>{
     response.render("spots/newSpot.ejs");
 }); 
-// Post route for spots
-app.post("/spots", async (request, response,next) =>{
+
+// Joi validation middleware 
+
+const validateSpot = (request, response, next) => {
+    // joi validation
+    const { error } = spotSchema.validate(request.body);
+    if (error) {
+        const message = error.details.map(element => element.message).join(",");
+        throw new EndpointError(message, 400);
+    } else {
+        next(); // Move on to the actual route handler
+    }
+};
+
+// (POST) Spots endpoint
+// validateSpot is a middleware 
+app.post("/spots", validateSpot, async (request, response,next) =>{
     try{
-        // Joi schema and requirements
-        const spotSchema = Joi.object({
-            spot : Joi.object({
-                title: Joi.string().required(),
-                location: Joi.string().required,
-                description: Joi.string().required(),
-                image: Joi.string().required()
-
-            }).required()
-        });
-        // validation
-        const {error} = spotSchema.validate(request.body);
-        if(error){
-            const message = error.details.map(element => element.message).join(",");
-            throw new EndpointError(message, 400);
-        }
-
         const spot = new Spot(request.body.spot);
         await spot.save();
         response.redirect(`/spots/${spot._id}`);
@@ -86,12 +85,12 @@ app.post("/spots", async (request, response,next) =>{
         next(error);
     }
 });
-// Get an Spot by ID
+// (GET) Spot by ID endpoint
 app.get("/spots/:id", async (request, response, next) =>{
     try{
         const spot = await Spot.findById(request.params.id);
         if(!spot){
-            throw new EndPointError("Spot not found ", 404);
+            throw new EndpointError("Spot not found ", 404);
         }
         console.log(spot);
         response.render("spots/showSpots.ejs", {spot});
@@ -99,7 +98,7 @@ app.get("/spots/:id", async (request, response, next) =>{
         next(error);
     }
 });
-// Get  form to edit an Spot by ID
+// (GE Form to edit an Spot by ID endpoint
 app.get("/spots/:id/edit", async (request, response, next) =>{
     try{
         const spot = await Spot.findById(request.params.id);
@@ -109,17 +108,18 @@ app.get("/spots/:id/edit", async (request, response, next) =>{
         next(error);
     }
 });
-
-app.put("/spots/:id", async (request, response, next) =>{
+// (PUT) Update a spot by ID endpoint
+app.put("/spots/:id", validateSpot, async (request, response, next) =>{
     try{
         const {id} = request.params;
-        const spot = await Spot.findByIdAndUpdate(id, {...request.body.spot});
+        const spot = await Spot.findByIdAndUpdate(id, { ...request.body.spot }, { new: true });
         response.redirect(`/spots/${spot._id}`);
     }catch(error){
         next(error);
     }
 });
 
+// (DELETE) Delete spot by ID endpoint
 app.delete("/spots/:id", async (request, response, next) =>{
     try{
         const {id} = request.params;
@@ -130,6 +130,38 @@ app.delete("/spots/:id", async (request, response, next) =>{
     }
 });
 
+// Joi validation middleware for Reviews
+const validateReview = (request, response, next) => {
+    const { error } = reviewSchema.validate(request.body);
+    if (error) {
+        const message = error.details.map(element => element.message).join(",");
+        throw new EndpointError(message, 400);
+    } else {
+        next();
+    }
+};
+
+// (POST) Review for a spot endpoint
+// validateReview is a middleware
+app.post("/spots/:id/reviews",validateReview, async (request, response, next) =>{
+    try{
+        // Find the spot first
+        const spot = await Spot.findById(request.params.id);
+        // Create a review
+        const review = new Review(request.body.review);
+        // Add the review to the Spot
+        spot.reviews.push(review);
+        await review.save();
+        await spot.save();
+        
+        // Redirect to the spot page
+        response.redirect(`/spots/${spot._id}`);
+    }catch(error){
+        console.log(error)
+    }
+});
+
+// Default response
 app.all("/{*path}", (request, response, next) =>{
     next(new EndpointError("Page not found", 404));
 });
